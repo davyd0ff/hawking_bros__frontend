f76a7cf2aba372ebacd77a4ae09084dc3e131e32
Ark Davyd0ff <ark.davyd0ff@gmail.com>
2022-05-24 22:11:44 +1000

    add then for await json-response

--- lesson3/scripts.js
+++ lesson3/scripts.js
@@ -6,13 +6,15 @@ loadBtn.addEventListener("click", function (evt) {
   event.preventDefault();
 
   const searchValue = searchInput.value.trim().toLowerCase();
-  fetch(`https://api.github.com/users/${searchValue}`).then(
-    (data) =>
-      (resultsContainer.innerHTML = `<div class="response-container">
+  fetch(`https://api.github.com/users/${searchValue}`)
+    .then((response) => response.json())
+    .then(
+      (data) =>
+        (resultsContainer.innerHTML = `<div class="response-container">
                 <img src="${data.avatar_url}">
                 <p> Имя: <span>${data.name}</span><p>
                 <p> О себе: <span>${data.bio}</span><p>
                 <p> Кол-во репозиториев: <span>${data.public_repos}</span><p>
         </div>`)
-  );
+    );
 });
