54454e596b663a2542292a7cd07603e38182a0da
Ark Davyd0ff <ark.davyd0ff@gmail.com>
2022-05-31 14:36:26 +1000

    task init

---
+++ lesson4/task#2/index.js
@@ -0,0 +1,19 @@
+/*
+  Переписатьпримерлотереинаasync/await.
+*/
+
+function lottery() {
+  console.log("Вы начали игру");
+  let promise = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      Math.random(0) > 0.5 ? resolve() : reject("Вы промахнулись");
+    }, 1000);
+  });
+  return promise;
+}
+
+lottery()
+  .then(() => console.log("Вы выиграли"))
+  .then(() => console.log("Вам заплатили"))
+  .catch(() => console.log("Вы проиграли"))
+  .finally(() => console.log("Игра закончена"));
