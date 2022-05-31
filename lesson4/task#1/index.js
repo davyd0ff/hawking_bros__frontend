28b3c9fafa06f169f39da1e58b32683f2bb9c066
Ark Davyd0ff <ark.davyd0ff@gmail.com>
2022-05-31 16:06:25 +1000

    correction: checking if the value is a number

--- lesson4/task#1/index.js
+++ lesson4/task#1/index.js
@@ -8,16 +8,24 @@
 const input = document.getElementById("input");
 const messagePlaceholder = document.getElementById("message-placeholder");
 
-const validate = (inputValue) => {
-  if (inputValue == "") throw new Error("value is empty");
-  if (typeof inputValue != Number) throw new Error("value isn't number");
-  if (inputValue < 5) throw new Error("value less than 5");
-  if (inputValue > 10) throw new Error("value greater than 10");
+const isNumber = (value) => {
+  if (value instanceof Number) {
+    value = value.instanceof();
+  }
+  return isFinite(value) && !isNaN(parseFloat(value));
+};
+
+const validate = (value) => {
+  if (value === "") throw new Error("value is empty");
+  if (!isNumber(value)) throw new Error("value isn't Number");
+  if (value < 5) throw new Error("value less than 5");
+  if (value > 10) throw new Error("value grater than 10");
 };
 
 input.addEventListener("change", (env) => {
   try {
     validate(env.target.value);
+    messagePlaceholder.innerText = "Ok";
   } catch (error) {
     messagePlaceholder.innerText = error;
   }
