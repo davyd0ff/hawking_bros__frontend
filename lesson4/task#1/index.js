6b77fbc2a3133005b28f52e764a084b68bcc6961
Ark Davyd0ff <ark.davyd0ff@gmail.com>
2022-05-31 16:01:40 +1000

    solution for task#1

--- lesson4/task#1/index.js
+++ lesson4/task#1/index.js
@@ -4,3 +4,21 @@
   ошибку с соответствующим текстом. Чтобы пользователю было понятен текст ошибки, выводите текст ошибки в 
   абзац под инпутом.
 */
+
+const input = document.getElementById("input");
+const messagePlaceholder = document.getElementById("message-placeholder");
+
+const validate = (inputValue) => {
+  if (inputValue == "") throw new Error("value is empty");
+  if (typeof inputValue != Number) throw new Error("value isn't number");
+  if (inputValue < 5) throw new Error("value less than 5");
+  if (inputValue > 10) throw new Error("value greater than 10");
+};
+
+input.addEventListener("change", (env) => {
+  try {
+    validate(env.target.value);
+  } catch (error) {
+    messagePlaceholder.innerText = error;
+  }
+});
